4e6abe29e0bfa3bc8966b0804f13745207b65a88
JefO <jef.ogu@gmail.com>
2023-02-03 15:08:06 -0500

    added JS to Projects

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var topic = 'HTML';
+// if (topic === 'HTML') {
+//   console.log("Lets study HTML!")
+// } else if (topic === 'CSS') {
+//   console.log("Lets study CSS!")
+// } else if (topic === 'Git') {
+//   console.log("Lets study Git!")
+// } else if (topic === 'JavaScript') {
+//   console.log("Lets study JavaScript!")
+// } else {
+//   console.log("Please try again!")
+// }
+
+for (var i = 0; i < topics.length; i++) {
+  console.log(topics[i])
+}
