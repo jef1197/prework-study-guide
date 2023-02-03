6243830a84a763724108bc5bf4f192524dfaba88
JefO <jef.ogu@gmail.com>
2023-02-03 15:16:11 -0500

    added functions to project

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-var topic = 'HTML';
-// if (topic === 'HTML') {
-//   console.log("Lets study HTML!")
-// } else if (topic === 'CSS') {
-//   console.log("Lets study CSS!")
-// } else if (topic === 'Git') {
-//   console.log("Lets study Git!")
-// } else if (topic === 'JavaScript') {
-//   console.log("Lets study JavaScript!")
-// } else {
-//   console.log("Please try again!")
-// }
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var i = 0; i < topics.length; i++) {
-  console.log(topics[i])
+function lsitTopics() {
+  for (var i = 0; i < topics.length; i++) {
+    console.log(topics[i])
+  }
 }
+
+function selectTopic() {
+  if (randomTopic === 'HTML') {
+    console.log("Lets study HTML!")
+  } else if (randomTopic === 'CSS') {
+    console.log("Lets study CSS!")
+  } else if (randomTopic === 'Git') {
+    console.log("Lets study Git!")
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Lets study JavaScript!")
+  } else {
+    console.log("Please try again!")
+  }
+}
+
+console.log('Here are the topics we learned through Prework:');
+lsitTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
